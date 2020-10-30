9ba5595240c384e621fc2912929d40e2c574b6be
Debjeetg1 <73057571+Debjeetg1@users.noreply.github.com>
2020-10-30 13:35:09 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,24 @@
+class Box
+{
+    constructor(x , y  , width , height)
+    {
+        var box_options = 
+        {
+            isStatic: true
+        }
+        
+        this.body = Bodies.rectangle(x , y , width , height, box_options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world , this.body);
+    }
+
+    display()
+    {
+       var position = this.body.position;
+
+       fill("red");
+       rect(position.x , position.y , this.width , this.height);
+    }
+}
